9156c6155c18be86ad008ef09b61c3bf6efbf763
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-09-24 22:23:38 +0800

    LeetCode Training: 14. Longest Common Prefix is accepted

---
+++ LeetCode/Training/14. Longest Common Prefix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonPrefix(self, strPrefix):
+        if strPrefix == []:
+            return ""
+            
+        strCommon = strPrefix[0]
+        for i in range(len(strCommon)):
+            for j in range(1, len(strPrefix)):
+                try:
+                    if strCommon[i] != strPrefix[j][i]:
+                        if i == 0:
+                            return ""
+                        else:
+                            return strCommon[:i]
+                except:
+                    if i == 0:
+                        return ""
+                    else:
+                        return strCommon[:i]                    
+        return strCommon
+        
