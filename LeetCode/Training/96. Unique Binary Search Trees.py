8fe0639b8070fc58a535feeeb2ecee9e910e1dca
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-01 20:53:22 +0800

    96. Unique Binary Search Trees: Accepted

--- LeetCode/Training/96. Unique Binary Search Trees.py
+++ LeetCode/Training/96. Unique Binary Search Trees.py
@@ -6,12 +6,22 @@
 
 class Solution:
     def numTrees(self, n: int) -> int:
+        stack = {}
+
         def count_bst(min_val, max_val):
             if min_val >= max_val:
                 return 1
+
+            key = (min_val, max_val)
+            if key in stack:
+                return stack[key]
+
             no_of_trees = 0
             for i in range(min_val, max_val + 1):
                 no_of_trees += count_bst(min_val, i - 1) * count_bst(i + 1, max_val)
+
+            stack[key] = no_of_trees
+
             return no_of_trees
 
         return count_bst(1, n)
