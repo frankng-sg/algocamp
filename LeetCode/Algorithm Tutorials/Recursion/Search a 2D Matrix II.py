25a889f25c814f25e342cd4d6d3894ffdb01e372
Nguyen, Frank <frank.nguyen@thermofisher.com>
2019-10-05 11:56:39 +0800

    Search a 2D Matrix II (non-recursion) with a different approach. This approach is much simpler and easy to understand. Running time is faster

--- LeetCode/Algorithm Tutorials/Recursion/Search a 2D Matrix II.py
+++ LeetCode/Algorithm Tutorials/Recursion/Search a 2D Matrix II.py
@@ -1,58 +1,37 @@
 class Solution:
 
-    @staticmethod
-    def binary_search(target, matrix, row=None, col=None):
-        if row is None:
-            low = 0
-            high = len(matrix) - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if target == matrix[mid][col]:
-                    return True
-                if target < matrix[mid][col]:
-                    high = mid - 1
-                if target > matrix[mid][col]:
-                    low = mid + 1
-            return False
-        if col is None:
-            low = 0
-            high = len(matrix[0]) - 1
-            while low <= high:
-                mid = (low + high) // 2
-
-                if target == matrix[row][mid]:
-                    return True
-                if target < matrix[row][mid]:
-                    high = mid - 1
-                if target > matrix[row][mid]:
-                    low = mid + 1
-            return False
-
-        return False
-
     def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
         if not matrix:
             return False
 
-        search_row = len(matrix) - 1
-        search_col = len(matrix[0]) - 1
-        while search_row >= 0 and search_col >= 0:
+        max_row = len(matrix) - 1
+        max_col = len(matrix[0]) - 1
+        search_row = 0
+        search_col = max_col
+
+        while search_row <= max_row and search_col >= 0:
             if matrix[search_row][search_col] is target:
                 return True
-            if self.binary_search(target, matrix, row=search_row):
-                return True
-            if self.binary_search(target, matrix, col=search_col):
-                return True
-            search_row -= 1
-            search_col -= 1
+            if matrix[search_row][search_col] > target:
+                search_col -= 1
+            else:
+                search_row += 1
+
         return False
 
 
 func = Solution()
-print(func.searchMatrix(
-    [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]], 20))
+
+# Output: False
+matrix = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]]
+target = 20
+print(func.searchMatrix(matrix, target))
+
+# Output: True
+matrix = [[48, 65, 70, 113, 133, 163, 170, 216, 298, 389], [89, 169, 215, 222, 250, 348, 379, 426, 469, 554],
+          [178, 202, 253, 294, 367, 392, 428, 434, 499, 651], [257, 276, 284, 332, 380, 470, 516, 561, 657, 698],
+          [275, 331, 391, 432, 500, 595, 602, 673, 758, 783], [357, 365, 412, 450, 556, 642, 690, 752, 801, 887],
+          [359, 451, 534, 609, 654, 662, 693, 766, 803, 964], [390, 484, 614, 669, 684, 711, 767, 804, 857, 1055],
+          [400, 515, 683, 732, 812, 834, 880, 930, 1012, 1130], [480, 538, 695, 751, 864, 939, 966, 1027, 1089, 1224]]
+target = 642
+print(func.searchMatrix(matrix, target))
