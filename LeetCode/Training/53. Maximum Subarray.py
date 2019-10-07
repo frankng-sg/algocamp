1258c07db0d29dff5c5a4e24fcb153f3c689d67c
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-07 22:56:41 +0800

    53. Maximum Subarray Complexity: O(n) Runtime: 80ms

--- LeetCode/Training/53. Maximum Subarray.py
+++ LeetCode/Training/53. Maximum Subarray.py
@@ -6,45 +6,32 @@
 # Output: 6
 # Explanation: [4,-1,2,1] has the largest sum = 6.
 class Solution:
-    def find_max_cross_sum(self, nums, low, mid, high):
-        max_sum = sum = nums[mid]
-        for i in range(mid + 1, high + 1):
-            sum += nums[i]
-            if max_sum < sum:
-                max_sum = sum
-
-        sum = max_sum
-        for i in range(mid - 1, -1, -1):
-            sum += nums[i]
-            if max_sum < sum:
-                max_sum = sum
-
-        return max_sum
-
-    def recur_search(self, nums, low, high):
-        if low == high:
-            return nums[low]
-
-        mid = (low + high) // 2
-        left_sum = self.recur_search(nums, low, mid)
-        right_sum = self.recur_search(nums, mid + 1, high)
-        cross_sum = self.find_max_cross_sum(nums, low, mid, high)
-        return max(left_sum, right_sum, cross_sum)
-
     def maxSubArray(self, nums):
-        return self.recur_search(nums, 0, len(nums) - 1)
-
+        gain = 0
+        loss = 0
+        max_gain = nums[0]
+        for i in range(0, len(nums)):
+            if nums[i] < 0:
+                loss += nums[i]
+                max_gain = max(max_gain, nums[i])
+            else:
+                if gain + loss >= 0:
+                    gain = nums[i] + loss + gain
+                    loss = 0
+                else:
+                    gain = nums[i]
+                    loss = 0
+                max_gain = max(max_gain, gain)
+        return max_gain
 
 # func = Solution()
 #
-# nums = [0, 5, 0, 0, 0, 1, 2, 3, 4]
-# print(func.maxSubArray(nums))  # Output: 15
-
-# nums = [-2, -7, -3, -4, -10, -2, -1, -5, -4]
-# print(func.maxSubArray(nums))  # Output: -1
-
 # nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 # print(func.maxSubArray(nums))  # Output: 6
-
+#
+# nums = [0, 5, 0, 0, 0, 1, 2, 3, 4]
+# print(func.maxSubArray(nums))  # Output: 15
+# nums = [-2, -7, -3, -4, -10, -2, -1, -5, -4]
+# print(func.maxSubArray(nums))  # Output: -1
 # nums = [2,0,3,-2]
 # print(func.maxSubArray(nums))  # Output: 5
