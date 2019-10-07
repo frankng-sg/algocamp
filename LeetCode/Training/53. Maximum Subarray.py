97afb29caf4cd8d3355b1cc9a2caadb116b44afd
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-07 23:02:27 +0800

    53. Maximum Subarray Complexity: O(n) Simple and Elegant Approach

--- LeetCode/Training/53. Maximum Subarray.py
+++ LeetCode/Training/53. Maximum Subarray.py
@@ -5,24 +5,18 @@
 # Input: [-2,1,-3,4,-1,2,1,-5,4],
 # Output: 6
 # Explanation: [4,-1,2,1] has the largest sum = 6.
+
+
 class Solution:
     def maxSubArray(self, nums):
-        gain = 0
-        loss = 0
-        max_gain = nums[0]
-        for i in range(0, len(nums)):
-            if nums[i] < 0:
-                loss += nums[i]
-                max_gain = max(max_gain, nums[i])
-            else:
-                if gain + loss >= 0:
-                    gain = nums[i] + loss + gain
-                    loss = 0
-                else:
-                    gain = nums[i]
-                    loss = 0
-                max_gain = max(max_gain, gain)
-        return max_gain
+        current_sum = 0
+        max_sum = nums[0]
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            if max_sum < current_sum:
+                max_sum = current_sum
+
+        return max_sum
 
 # func = Solution()
 #
