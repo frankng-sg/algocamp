cbbe5e9ba1df276a8bf5414def0c9da4961e6a6b
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-17 23:25:25 +0800

    100. Same Tree Algorithm: Iteration

--- LeetCode/Training/100. Same Tree.java
+++ LeetCode/Training/100. Same Tree.java
@@ -1,23 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+// Definition for a binary tree node.
+ public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        }
-        if (p == null || q == null) {
-            return false;
-        }
-        if (p.val != q.val) {
-            return false;
-        }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    	Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode node1, node2;
+        stack.push(p);
+      	stack.push(q);
+      	do {
+          	node1 = stack.pop();
+          	node2 = stack.pop();
+          	if (node1 == null && node2 == null) {
+             	continue;
+            }
+          	if (node1 == null || node2 == null) {
+              	return false;
+            }
+          	if (node1.val != node2.val) {
+              	return false;
+            }
+
+          	stack.push(node1.left);
+          	stack.push(node2.left);
+          	stack.push(node1.right);
+          	stack.push(node2.right);
+
+        } while (!stack.empty());
+
+		return true;
     }
 }
