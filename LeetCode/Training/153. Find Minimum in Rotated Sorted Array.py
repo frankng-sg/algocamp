9845f6397fe3e7b66dca0d4473f53fbf4b37c83d
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-28 17:23:02 +0800

    153. Find Minimum in Rotated Sorted Array Time:32 ms, faster than 22.75% Space:11.9 MB, less than 71.43%

--- LeetCode/Training/153. Find Minimum in Rotated Sorted Array.py
+++ LeetCode/Training/153. Find Minimum in Rotated Sorted Array.py
@@ -1,24 +1,18 @@
 class Solution(object):
     def findMin(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums) == 0 or nums is None:
-            return None
-
         if len(nums) == 1:
             return nums[0]
-
         left, right = 0, len(nums) - 1
-        while left + 1 < right:
+        while left < right:
             mid = (left + right) // 2
-            if nums[left] > nums[mid]:
-                right = mid
-            else:
+            if nums[mid] > nums[mid + 1]:
+                return nums[mid + 1]
+            if nums[mid] > nums[right]:
                 left = mid
+            else:
+                right = mid
 
-        return min(nums[0], nums[left], nums[right])
+        return nums[left]
 
 func = Solution()
 
