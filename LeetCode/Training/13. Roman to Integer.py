896667bf85bdf533da832db8eb6ada84bacc3c46
Frank Nguyen <frank.nguyen@thermofisher.com>
2020-03-22 20:01:51 +0800

    Create 13. Roman to Integer.py

--- LeetCode/Training/13. Roman to Integer.py
+++ LeetCode/Training/13. Roman to Integer.py
@@ -1,10 +1,18 @@
 class Solution:
     def romanToInt(self, roman_num):
-        rom2int = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        int_num = rom2int[roman_num[0]]
-        for i in range(1, len(roman_num)):
-            if rom2int[roman_num[i]] <= rom2int[roman_num[i-1]]:
-                int_num += rom2int[roman_num[i]]
+        roman2int = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        num_len = len(roman_num)
+        result = roman2int[roman_num[num_len - 1]]
+        for i in range(num_len - 1, 0, -1):
+            if roman2int[roman_num[i]] > roman2int[roman_num[i - 1]]:
+                result -= roman2int[roman_num[i - 1]]
             else:
-                int_num += rom2int[roman_num[i]] - 2 * rom2int[roman_num[i-1]]
-        return int_num
+                result += roman2int[roman_num[i - 1]]
+
+        return result
+
+ans = Solution()
+
+print(ans.romanToInt("III"))  # 3
+print(ans.romanToInt("XCVI")) # 96
+print(ans.romanToInt("IV"))  # 4
