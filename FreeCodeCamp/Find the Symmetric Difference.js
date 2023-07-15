7f85ffe432ea99617624700850a27db9dd1df52a
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-07-15 12:38:36 +0800

    Solve Find the Symmetric Difference

--- FreeCodeCamp/Find the Symmetric Difference.js
+++ FreeCodeCamp/Find the Symmetric Difference.js
@@ -1,5 +1,23 @@
-function sym(args) {
-    return args;
+function sym(...args) {
+    const count = {};
+    const symDiff = [];
+
+    args.forEach((list) => {
+        const uniqList = Array.from(new Set(list));
+        uniqList.forEach((key) => {
+            if (key in count) count[key] += 1;
+            else count[key] = 1;
+        });
+    });
+
+    for (const key in count) {
+        if (count[key] % 2 == 1) {
+            symDiff.push(parseInt(key));
+        }
+    }
+    return symDiff;
 }
 
-console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+console.log(sym([1, 2, 3], [5, 2, 1, 4])); // 3, 4, 5
+console.log(sym([1, 2, 3, 3], [5, 2, 1, 4])); //3, 4, 5
+console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5])); //1, 4, 5
