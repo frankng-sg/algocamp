aae42ea153ff543b0e668aa6ac8edf349bb8d9f2
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-07-22 18:59:32 +0800

    Implement Le Minh Hoang version of QuickSort

--- FreeCodeCamp/QuickSort.js
+++ FreeCodeCamp/QuickSort.js
@@ -1,34 +1,28 @@
-function swap(arr, i, j) {
-    let tmp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = tmp;
-}
-
 function partition(arr, l, r) {
-    let pivot = arr[r];
-    let i = l - 1;
-    let j;
-    for (j = l; j <= r - 1; j++)
-        if (arr[j] < pivot) {
-            i += 1;
-            swap(arr, i, j);
+    if (l >= r) return;
+    let pivot = arr[l + ((r - l) >> 1)];
+    let i = l;
+    let j = r;
+    while (i <= j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+        if (i <= j) {
+            let tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            i++;
+            j--;
         }
-    i += 1;
-    swap(arr, i, r);
-    return i;
-}
-
-function qSort(arr, l, r) {
-    if (l >= r || l < 0) return;
-    let p = partition(arr, l, r);
-    qSort(arr, l, p - 1);
-    qSort(arr, p + 1, r);
+    }
+    partition(arr, l, j);
+    partition(arr, i, r);
 }
 
 function quickSort(arr) {
     // Only change code below this line
-    qSort(arr, 0, arr.length - 1);
+    partition(arr, 0, arr.length - 1);
     return arr;
     // Only change code above this line
 }
-console.log(quickSort([3, 2, 1, 4, 5, 4]));
+
+console.log(quickSort([3, 2, 4, 4, 5, 4]));
