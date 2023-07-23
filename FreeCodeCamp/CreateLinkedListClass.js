2288b2eb3814a0c404ba1550166a30160d69e1f7
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-07-23 19:00:28 +0800

    Add remove()

--- FreeCodeCamp/CreateLinkedListClass.js
+++ FreeCodeCamp/CreateLinkedListClass.js
@@ -7,29 +7,55 @@ function LinkedList() {
         this.next = null;
     };
 
-    this.head = function () {
-        return head;
-    };
-
     this.size = function () {
         return length;
     };
 
+    this.head = function () {
+        return head;
+    };
+
     this.add = function (element) {
-        var newNode = new Node(element);
-        if (!head) head = newNode;
-        else {
-            let node = head;
-            while (node.next !== null) node = node.next;
-            node.next = newNode;
+        var node = new Node(element);
+        if (head === null) {
+            head = node;
+        } else {
+            var currentNode = head;
+
+            while (currentNode.next) {
+                currentNode = currentNode.next;
+            }
+
+            currentNode.next = node;
         }
+
         length++;
+    };
+
+    this.remove = function (element) {
+        if (!head) return;
+        if (head.element === element) {
+            head = head.next;
+            length--;
+            return;
+        }
+        let prev = head;
+        let curr = head.next;
+        while (curr) {
+            if (curr.element === element) {
+                prev.next = curr.next;
+                length--;
+                break;
+            }
+            prev = curr;
+            curr = curr.next;
+        }
     };
 }
 
 let ll = new LinkedList();
 ll.add(1);
-ll.add(2);
-ll.add(3);
-console.log(ll.head().element);
-console.log(ll.head().next);
+console.log(ll.size());
+ll.remove(1);
+ll.remove(2);
+console.log(ll.size());
