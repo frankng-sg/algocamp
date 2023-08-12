2a6d552dedf4da6e43b2dba71f0117edf2f19185
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-12 14:01:58 +0800

    Solve with goroutine

--- LeetCode/49. Group Anagrams.go
+++ LeetCode/49. Group Anagrams.go
@@ -1,6 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+func groupAnagramsWithGoroutines(strs []string) [][]string {
+	m := make(map[[26]int][]string)
+	var mLock sync.Mutex
+	var wg sync.WaitGroup
+	for _, s := range strs {
+		wg.Add(1)
+		go func(str string) {
+			defer wg.Done()
+			var count [26]int
+			for _, ch := range str {
+				count[ch-'a']++
+			}
+			mLock.Lock()
+			m[count] = append(m[count], str)
+			mLock.Unlock()
+		}(s)
+	}
+	wg.Wait()
+	result := make([][]string, len(m))
+	i := 0
+	for _, r := range m {
+		result[i] = r
+		i++
+	}
+	return result
+}
 
 func groupAnagrams(strs []string) [][]string {
 	m := make(map[[26]int][]string)
