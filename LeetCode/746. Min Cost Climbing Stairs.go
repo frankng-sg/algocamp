745d7b53cec1379586ae5feca98e904e37efa246
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-13 18:08:53 +0800

    Refactor code Run:100% Mem:84%

--- LeetCode/746. Min Cost Climbing Stairs.go
+++ LeetCode/746. Min Cost Climbing Stairs.go
@@ -11,13 +11,10 @@ func lowerInt(a, b int) int {
 
 func minCostClimbingStairs(cost []int) int {
 	n := len(cost)
-	sum := make([]int, n+1)
-	sum[0] = 0
-	sum[1] = 0
-	for i := 2; i <= n; i++ {
-		sum[i] = lowerInt(sum[i-1]+cost[i-1], sum[i-2]+cost[i-2])
+	for i := 2; i < n; i++ {
+		cost[i] = cost[i] + lowerInt(cost[i-1], cost[i-2])
 	}
-	return sum[n]
+	return lowerInt(cost[n-1], cost[n-2])
 }
 
 func main() {
