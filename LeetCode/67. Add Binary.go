908ee0a32e92f6923d1edb2f6e509779786e6b61
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-13 18:50:09 +0800

    Refactor code Run:100 Mem:82

--- LeetCode/67. Add Binary.go
+++ LeetCode/67. Add Binary.go
@@ -2,37 +2,39 @@ package main
 
 import "fmt"
 
+func higherInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func addBinary(a string, b string) string {
-	result := ""
-	var c, da, db byte
-	for i, j := len(a)-1, len(b)-1; ; {
+	la, lb := len(a), len(b)
+	lm := higherInt(la, lb)
+	result := make([]byte, lm+1)
+	var c byte
+	for i, j, k := la-1, lb-1, lm; k >= 0; k-- {
+		sum := c
 		if i >= 0 {
-			da = a[i] - '0'
-		} else {
-			da = 0
+			sum += a[i] - '0'
+			i--
 		}
 		if j >= 0 {
-			db = b[j] - '0'
-		} else {
-			db = 0
+			sum += b[j] - '0'
+			j--
 		}
-		sum := c + da + db
-		if sum == 0 && i < 0 && j < 0 {
-			break
-		}
-		result = string(sum&1+'0') + result
+		result[k] = sum&1 + '0'
 		c = sum >> 1
-		i--
-		j--
 	}
-	if result == "" {
-		result = "0"
+	if result[0] == '0' {
+		return string(result[1:])
 	}
-	return result
+	return string(result)
 }
 
 func main() {
-	fmt.Println(addBinary("100", "110010")) // Output: "0"
+	fmt.Println(addBinary("100", "110010")) // Output: "110110"
 	fmt.Println(addBinary("0", "0"))        // Output: "0"
 	fmt.Println(addBinary("0", "1"))        // Output: "1"
 	fmt.Println(addBinary("1", "1"))        // Output: "10"
