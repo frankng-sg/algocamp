f9a061198a9fed9e917ffef6d28183a91b2c62cb
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-16 12:40:50 +0800

    Solve https://leetcode.com/problems/redundant-connection Run:88 Mem:35

---
+++ LeetCode/684. Redundant Connection.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func findRedundantConnection(edges [][]int) []int {
+	parent := make(map[int]int)
+	root := func(node int) int {
+		for parent[node] != node {
+			node = parent[node]
+		}
+		return node
+	}
+	for _, edge := range edges {
+		u := edge[0]
+		v := edge[1]
+		if parent[u] == 0 && parent[v] == 0 {
+			parent[u] = u // root
+		}
+		if parent[v] == 0 {
+			parent[v] = root(u)
+		} else if parent[u] == 0 {
+			parent[u] = root(v)
+		} else if root(u) == root(v) {
+			return edge
+		} else {
+			parent[root(v)] = root(u)
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	fmt.Println(findRedundantConnection([][]int{{1, 2}, {2, 3}, {3, 4}, {1, 4}, {1, 5}})) // Output: [1 4]
+	fmt.Println(findRedundantConnection([][]int{{3, 4}, {1, 2}, {2, 4}, {3, 5}, {2, 5}})) // Output: [2 5]
+}
