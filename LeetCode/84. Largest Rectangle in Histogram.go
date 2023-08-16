0d39f99a068446c1641fbb6a403e86d918d075ee
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-16 23:00:00 +0800

    Refactor code

--- LeetCode/84. Largest Rectangle in Histogram.go
+++ LeetCode/84. Largest Rectangle in Histogram.go
@@ -2,24 +2,22 @@ package main
 
 import "fmt"
 
+func higherInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func largestRectangleArea(heights []int) int {
-	maxh := []int{0}
-	pos := []int{0}
+	maxh := []int{}
+	pos := []int{}
 	n := len(heights)
 	maxArea := 0
 	for i := 0; i < n; i++ {
-		if heights[i] > maxArea {
-			maxArea = heights[i]
-		}
-		found := false
 		for j := len(maxh) - 1; j >= 0; j-- {
 			if heights[i] < maxh[j] {
-				found = true
 				maxh[j] = heights[i]
-			}
-			area := maxh[j] * (i - pos[j] + 1)
-			if area > maxArea {
-				maxArea = area
 			}
 		}
 		for j := len(maxh) - 2; j >= 0; j-- {
@@ -28,17 +26,22 @@ func largestRectangleArea(heights []int) int {
 				pos = pos[:j+1]
 			}
 		}
-		if !found {
+		if len(maxh) == 0 || heights[i] > maxh[len(maxh)-1] {
 			maxh = append(maxh, heights[i])
 			pos = append(pos, i)
+		}
+		for j := len(maxh) - 1; j >= 0; j-- {
+			maxArea = higherInt(maxArea, maxh[j]*(i-pos[j]+1))
 		}
 	}
 	return maxArea
 }
 
 func main() {
-	fmt.Println(largestRectangleArea([]int{1}))                               // Output: 1
-	fmt.Println(largestRectangleArea([]int{2, 1, 5, 6, 2, 3}))                // Output: 10
-	fmt.Println(largestRectangleArea([]int{2, 4}))                            // Output: 4
-	fmt.Println(largestRectangleArea([]int{2, 1, 1, 1, 1, 5, 5, 1, 1, 1, 1})) // Output: 11
+	fmt.Println(largestRectangleArea([]int{9, 0}))                                                 // Output: 9
+	fmt.Println(largestRectangleArea([]int{1}))                                                    // Output: 1
+	fmt.Println(largestRectangleArea([]int{2, 1, 5, 6, 2, 3}))                                     // Output: 10
+	fmt.Println(largestRectangleArea([]int{2, 4}))                                                 // Output: 4
+	fmt.Println(largestRectangleArea([]int{2, 1, 1, 1, 1, 5, 5, 1, 1, 1, 1}))                      // Output: 11
+	fmt.Println(largestRectangleArea([]int{2, 1, 1, 1, 1, 5, 5, 9, 3, 2, 5, 7, 4, 4, 3, 6, 7, 8})) // Output: 26
 }
