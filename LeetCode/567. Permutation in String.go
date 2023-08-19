1e7bae64da2a6a30318c6f3f0256db49c6f6c9ef
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-19 11:01:40 +0800

    Refactor code Run:100 Mem:97

--- LeetCode/567. Permutation in String.go
+++ LeetCode/567. Permutation in String.go
@@ -3,25 +3,26 @@ package main
 import "fmt"
 
 func checkInclusion(s1 string, s2 string) bool {
-	target := [26]int{}
-	count := [26]int{}
 	n1 := len(s1)
 	n2 := len(s2)
 	if n2 < n1 {
 		return false
 	}
+	target := [26]int{}
+	count := [26]int{}
 	for i := 0; i < n1; i++ {
 		target[s1[i]-'a']++
 		count[s2[i]-'a']++
 	}
 	diff := 0
 	for i := 0; i < 26; i++ {
-		if target[i]-count[i] > 0 {
+		if count[i] < target[i] {
 			diff += target[i] - count[i]
 		}
 	}
 	l := 0
-	for r := n1 - 1; r < n2; {
+	r := n1 - 1
+	for {
 		if diff == 0 {
 			return true
 		}
@@ -36,6 +37,8 @@ func checkInclusion(s1 string, s2 string) bool {
 				diff -= 1
 			}
 			count[s2[r]-'a']++
+		} else {
+			break
 		}
 	}
 	return false
