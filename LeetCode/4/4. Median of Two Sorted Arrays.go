6acab930b95c24a0b2020dd6cf9f509af5538e61
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-21 21:42:19 +0800

    Solve median of two sorted arrays with binary search

--- LeetCode/4/4. Median of Two Sorted Arrays.go
+++ LeetCode/4/4. Median of Two Sorted Arrays.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func mergeArray(merge, arr1, arr2 []int) {
 	n1, n2 := len(arr1), len(arr2)
 	i, j := 0, 0
@@ -30,4 +32,46 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		return float64(merged[n>>1-1]+merged[n>>1]) / 2
 	}
 	return float64(merged[n/2])
+}
+
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	m, n := len(nums1), len(nums2)
+	imin, imax, mid := 0, m, (m+n+1)/2
+	for imin <= imax {
+		i := (imin + imax) >> 1
+		j := mid - i
+		if i < m && nums1[i] < nums2[j-1] {
+			imin = i + 1
+		} else if i > 0 && nums1[i-1] > nums2[j] {
+			imax = i - 1
+		} else {
+			maxOfLeft, minOfRight := 0., 0.
+			if i == 0 {
+				maxOfLeft = float64(nums2[j-1])
+			} else if j == 0 {
+				maxOfLeft = float64(nums1[i-1])
+			} else {
+				maxOfLeft = math.Max(float64(nums1[i-1]), float64(nums2[j-1]))
+				if (m+n)%2 == 1 {
+					return maxOfLeft
+				}
+			}
+			if (m+n)%2 == 1 {
+				return maxOfLeft
+			}
+			if i == m {
+				minOfRight = float64(nums2[j])
+			} else if j == n {
+				minOfRight = float64(nums1[i])
+			} else {
+				minOfRight = math.Min(float64(nums1[i]), float64(nums2[j]))
+			}
+			return (maxOfLeft + minOfRight) / 2
+		}
+	}
+
+	return 0.0
 }
