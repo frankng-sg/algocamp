b2add0653abbdb625a9f8c791d8cf0f9b3927289
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-27 22:46:25 +0800

    Add a new apporach to merge Run:69 Mem:99

--- LeetCode/88/88. Merge Sorted Array.go
+++ LeetCode/88/88. Merge Sorted Array.go
@@ -12,8 +12,27 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	sort.Ints(nums1)
 }
 
+func merge2(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+	for ; i >= 0 && j >= 0; k-- {
+		if nums1[i] <= nums2[j] {
+			nums1[k] = nums2[j]
+			j--
+		} else {
+			nums1[k] = nums1[i]
+			i--
+		}
+	}
+	for ; i >= 0; i, k = i-1, k-1 {
+		nums1[k] = nums1[i]
+	}
+	for ; j >= 0; j, k = j-1, k-1 {
+		nums1[k] = nums2[j]
+	}
+}
+
 func main() {
 	nums := []int{1, 2, 3, 0, 0, 0}
-	merge(nums, 3, []int{2, 5, 6}, 3)
+	merge2(nums, 3, []int{2, 5, 6}, 3)
 	fmt.Println(nums) // Output: [1,2,2,3,5,6]
 }
