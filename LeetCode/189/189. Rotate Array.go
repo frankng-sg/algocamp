b710d7b5a644aa7713d8e834d2990807785a179a
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-27 18:08:51 +0800

    Solve 189. Rotate Array with circular replacement Run:98 Mem:72

--- LeetCode/189/189. Rotate Array.go
+++ LeetCode/189/189. Rotate Array.go
@@ -18,10 +18,40 @@ func rotate(nums []int, k int) {
 	reverse(nums, k, len(nums)-1)
 }
 
+func rotate2(nums []int, k int) {
+	n := len(nums)
+	k = k % n
+	count := 0
+
+	for start := 0; count < n; start++ {
+		current, tmp1 := start, nums[start]
+		for {
+			next := (current + k) % n
+			tmp2 := nums[next]
+			nums[next] = tmp1
+			tmp1 = tmp2
+			current = next
+			count++
+
+			if start == current {
+				break
+			}
+		}
+	}
+}
+
 func main() {
 	var nums []int
 
 	nums = []int{1, 2, 3, 4, 5, 6, 7}
-	rotate(nums, 13)
+	rotate2(nums, 13)
 	fmt.Println(nums) // Output: [2 3 4 5 6 7 1]
+
+	nums = []int{-1, -100, 3, 99}
+	rotate2(nums, 2)
+	fmt.Println(nums) // Output: [3,99,-1,-100]
+
+	nums = []int{1, 2, 3, 4, 5, 6, 7}
+	rotate2(nums, 3)
+	fmt.Println(nums) // Output: [5,6,7,1,2,3,4]
 }
