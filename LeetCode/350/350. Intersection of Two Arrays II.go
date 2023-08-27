6b8f8303a90f74e4e16863a7ff547e3805a3af8c
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-08-27 14:58:58 +0800

    Solve with sort Run:100 Mem:97

--- LeetCode/350/350. Intersection of Two Arrays II.go
+++ LeetCode/350/350. Intersection of Two Arrays II.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // Time: O(m + n) Space: O(1)
 func intersect(nums1 []int, nums2 []int) []int {
@@ -21,6 +24,30 @@ func intersect(nums1 []int, nums2 []int) []int {
 	return output
 }
 
+// Time: O(Max(nlogn, m + n)) Space: O(1)
+func intersect2(nums1 []int, nums2 []int) []int {
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	output := make([]int, 0, len(nums1))
+	var i, j int
+	for ; i < len(nums1); i++ {
+		for ; j < len(nums2) && nums2[j] < nums1[i]; j++ {
+		}
+		if j >= len(nums2) {
+			break
+		}
+		if nums2[j] == nums1[i] {
+			output = append(output, nums2[j])
+			j++
+		}
+	}
+	return output
+}
+
 func main() {
-	fmt.Println(intersect([]int{1, 2, 2, 1}, []int{2, 2})) // Output: [2 2]
+	fmt.Println(intersect([]int{1, 2, 2, 1}, []int{2, 2}))  // Output: [2 2]
+	fmt.Println(intersect2([]int{1, 2, 2, 1}, []int{2, 2})) // Output: [2 2]
 }
