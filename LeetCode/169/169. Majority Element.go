203e81cbf35686743a846991a4881486a68bdc9b
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-09-02 22:32:09 +0800

    Solve using hash map Run:53 Mem:38

--- LeetCode/169/169. Majority Element.go
+++ LeetCode/169/169. Majority Element.go
@@ -5,8 +5,22 @@ import (
 	"sort"
 )
 
-// Time: O(nlogn), Space: O(1)
+// Time: O(n), Space: O(n)
 func majorityElement(nums []int) int {
+	count := make(map[int]int)
+	maxCount, maxVal := 1, nums[0]
+	for _, v := range nums {
+		count[v]++
+		if count[v] > maxCount {
+			maxCount = count[v]
+			maxVal = v
+		}
+	}
+	return maxVal
+}
+
+// Time: O(nlogn), Space: O(1)
+func majorityElement1(nums []int) int {
 	sort.Ints(nums)
 	count, maxCount, maxVal := 1, 1, nums[0]
 	for i := 1; i < len(nums); i++ {
@@ -27,7 +41,9 @@ func majorityElement(nums []int) int {
 }
 
 func main() {
-	fmt.Println(majorityElement([]int{3, 2, 3}))             // Output: 3
-	fmt.Println(majorityElement([]int{1, 0, 3, 2, 2, 2, 2})) // Output: 2
-	fmt.Println(majorityElement([]int{1}))                   // Output: 1
+	fmt.Println(majorityElement([]int{3, 2, 3}))                   // Output: 3
+	fmt.Println(majorityElement([]int{1, 0, 3, 2, 2, 2, 2}))       // Output: 2
+	fmt.Println(majorityElement([]int{1}))                         // Output: 1
+	fmt.Println(majorityElement([]int{1, 0, 2, 0, 3, 0, 4, 0, 0})) // Output: 0
+	fmt.Println(majorityElement([]int{1, 1, 1, 2, 2, 2, 2}))       // Output: 2
 }
