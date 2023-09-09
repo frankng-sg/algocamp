2cb1dcbde8c0c5d542d09007d00768778ebff72f
Frank Nguyen <dung.frank@ntucfirstcampus.com>
2023-09-09 20:46:41 +0800

    Solve 5.Longest Palindrome Run:100 Mem:65

--- LeetCode/5/5. Longest Palindromic Substring.go
+++ LeetCode/5/5. Longest Palindromic Substring.go
@@ -2,8 +2,41 @@ package main
 
 import "fmt"
 
-// Time: O(n^3), Space: O(1)
+// Time: O(n^2), Space: O(1)
 func longestPalindrome(s string) string {
+	if s == "" {
+		return ""
+	}
+	maxlen, maxstart := 1, 0
+	halflen := maxlen >> 1
+	expandPalStr := func(left, right int) (strstart, strlen int) {
+		for left >= 0 && right < len(s) && s[left] == s[right] {
+			left, right = left-1, right+1
+		}
+		left, right = left+1, right-1
+		return left, right - left + 1
+	}
+	for i := 0; i < len(s)-halflen; i++ {
+		var strstart, strlen int
+
+		strstart, strlen = expandPalStr(i-1, i)
+		if strlen > maxlen {
+			maxlen, halflen, maxstart = strlen, strlen>>1, strstart
+		}
+		strstart, strlen = expandPalStr(i, i+1)
+		if strlen > maxlen {
+			maxlen, halflen, maxstart = strlen, strlen>>1, strstart
+		}
+		strstart, strlen = expandPalStr(i, i)
+		if strlen > maxlen {
+			maxlen, halflen, maxstart = strlen, strlen>>1, strstart
+		}
+	}
+	return s[maxstart : maxstart+maxlen]
+}
+
+// Time: O(n^3), Space: O(1)
+func longestPalindrome1(s string) string {
 	if s == "" {
 		return ""
 	}
